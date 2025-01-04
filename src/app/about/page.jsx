1b0aff9a60f169dcd6e512f43a1fa8bf7f1fd3df
LeMinhLong2000 <leminhlong11b8@gmail.com>
2025-01-05 01:49:17 +0700

    About Page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,7 +1,13 @@
 "use client";
-import { motion } from "framer-motion";
-
+import Brain from "@/components/brain";
+import { motion, useScroll } from "framer-motion";
+import { useRef } from "react";
+ 
 const AboutPage = () => {
+  const containerRef = useRef()
+
+  const {scrollYProgress} = useScroll({container: containerRef})
+ 
   return (
     <motion.div
       className="h-full"
@@ -9,7 +15,205 @@ const AboutPage = () => {
       animate={{ y: "0%" }}
       transition={{ duration: 1 }}
     >
-      <div className="">AboutPage</div>
+      {/* CONTAINER */}
+      <div className="h-full overflow-scroll lg:flex">
+        {/* TEXT CONTAINER */}
+        <div className="p-4 sm:p-8 md:p-12 lg:p-20 xl:p-48 flex flex-col gap-24 md:gap-32 lg:gap-48 xl:gap-64 lg:w-2/3 lg:pr-0 xl:1/2">
+          {/* BIOGRAPHY CONTAINER */}
+          <div className="flex flex-col gap-12 justify-center">
+            <h1 className="font-boold text-2xl">BIOGRAPHY</h1>
+
+            <p className="text-lg">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi
+              ut eius aliquam totam dicta cupiditate ratione qui doloribus
+              pariatur. Tenetur quidem obcaecati perferendis voluptates illo
+              omnis velit deleniti vero nemo!
+            </p>
+            <span className="italic">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit.{" "}
+            </span>
+
+            {/* svg */}
+            <div className="self-end">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="100"
+                height="100"
+                viewBox="0 0 124 124"
+                fill="none"
+              >
+                <rect width="124" height="124" rx="24" fill="#F97316" />
+                <path
+                  d="M19.375 36.7818V100.625C19.375 102.834 21.1659 104.625 23.375 104.625H87.2181C90.7818 104.625 92.5664 100.316 90.0466 97.7966L26.2034 33.9534C23.6836 31.4336 19.375 33.2182 19.375 36.7818Z"
+                  fill="white"
+                />
+                <circle cx="63.2109" cy="37.5391" r="18.1641" fill="black" />
+                <rect
+                  opacity="0.4"
+                  x="81.1328"
+                  y="80.7198"
+                  width="17.5687"
+                  height="17.3876"
+                  rx="4"
+                  transform="rotate(-45 81.1328 80.7198)"
+                  fill="#FDBA74"
+                />
+              </svg>
+            </div>
+          </div>
+          {/* SKILLS CONTAINER */}
+          <div className="flex flex-col gap-12 justify-center">
+            <h1 className="font-boold text-2xl">SKILLS</h1>
+
+            {/* SKILL LIST */}
+            <div className="flex gap-4 flex-wrap">
+              <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                Java
+              </div>
+              <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                Java
+              </div>
+              <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                Java
+              </div>
+              <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                html
+              </div>
+
+              <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                mongo
+              </div>
+              <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                Java
+              </div>
+              <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                php
+              </div>
+              <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                Java
+              </div>
+
+              <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                Java
+              </div>
+              <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                nextjs
+              </div>
+              <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                Java
+              </div>
+              <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                Java
+              </div>
+            </div>
+          </div>
+          {/* EXPERIENCE CONTAINER */}
+          <div className="flex flex-col gap-12 justify-center">
+            <h1 className="font-boold text-2xl">EXPERIENCE</h1>
+            {/* EXPERIENCE LIST */}
+            <div className="">
+              {/* EXPERIENCE ITEM */}
+              <div className="flex justify-between h-48">
+                {/* LEFT */}
+                <div className="w-1/3 ">
+                  {/* JOB TITLE */}
+                  <div className="bg-white p-3 font-semibold rounded-b-lg rounded-s-lg">
+                    JOB TITLE 1
+                  </div>
+                  {/* JOB DES */}
+                  <div className="p-3 text-sm italic">JOB DES 1</div>
+                  {/* JOB DATE */}
+                  <div className="p-3 text-red-400 text-sm font-semibold">
+                    JOB DATE 1
+                  </div>
+                  {/* JOB COMPANY */}
+                  <div className="p-1 rounded bg-white text-sm font-semibold w-fit">
+                    APPLE
+                  </div>
+                </div>
+
+                {/* CENTER */}
+                <div className="w-1/6">
+                  {/* LINE */}
+                  <div className="w-1 h-full bg-gray-600 rounded relative">
+                    {/* LINE CIRCLE */}
+                    <div className="absolute w-5 h-5 rounded-full ring-4 ring-red-400 bg-white -left-2"></div>
+                  </div>
+                </div>
+                {/* RIGHT */}
+                <div className="w-1/3 "></div>
+              </div>
+              {/* EXPERIENCE ITEM */}
+              <div className="flex justify-between h-48">
+                {/* LEFT */}
+                <div className="w-1/3 "></div>
+
+                {/* CENTER */}
+                <div className="w-1/6">
+                  {/* LINE */}
+                  <div className="w-1 h-full bg-gray-600 rounded relative">
+                    {/* LINE CIRCLE */}
+                    <div className="absolute w-5 h-5 rounded-full ring-4 ring-red-400 bg-white -left-2"></div>
+                  </div>
+                </div>
+                {/* RIGHT */}
+                <div className="w-1/3 ">
+                  {/* JOB TITLE */}
+                  <div className="bg-white p-3 font-semibold rounded-b-lg rounded-s-lg">
+                    JOB TITLE 1
+                  </div>
+                  {/* JOB DES */}
+                  <div className="p-3 text-sm italic">JOB DES 1</div>
+                  {/* JOB DATE */}
+                  <div className="p-3 text-red-400 text-sm font-semibold">
+                    JOB DATE 1
+                  </div>
+                  {/* JOB COMPANY */}
+                  <div className="p-1 rounded bg-white text-sm font-semibold w-fit">
+                    APPLE
+                  </div>
+                </div>
+              </div>
+              {/* EXPERIENCE ITEM */}
+              <div className="flex justify-between h-48">
+                {/* LEFT */}
+                <div className="w-1/3 ">
+                  {/* JOB TITLE */}
+                  <div className="bg-white p-3 font-semibold rounded-b-lg rounded-s-lg">
+                    JOB TITLE 1
+                  </div>
+                  {/* JOB DES */}
+                  <div className="p-3 text-sm italic">JOB DES 1</div>
+                  {/* JOB DATE */}
+                  <div className="p-3 text-red-400 text-sm font-semibold">
+                    JOB DATE 1
+                  </div>
+                  {/* JOB COMPANY */}
+                  <div className="p-1 rounded bg-white text-sm font-semibold w-fit">
+                    APPLE
+                  </div>
+                </div>
+
+                {/* CENTER */}
+                <div className="w-1/6">
+                  {/* LINE */}
+                  <div className="w-1 h-full bg-gray-600 rounded relative">
+                    {/* LINE CIRCLE */}
+                    <div className="absolute w-5 h-5 rounded-full ring-4 ring-red-400 bg-white -left-2"></div>
+                  </div>
+                </div>
+                {/* RIGHT */}
+                <div className="w-1/3 "></div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* SVG CONTAINER */}
+        <div className="hidden lg:block w-1/3 sticky top-0 z-30 xl:1/2">
+        <Brain scrollYProgress={scrollYProgress}></Brain>
+        </div>
+      </div>
     </motion.div>
   );
 };
